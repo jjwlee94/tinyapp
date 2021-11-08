5b27075cb42fcb891cd795ab5734c99a193a3967
Jeewon Lee <jeewon.jlee@gmail.com>
2021-11-08 22:13:00 +0000

    Redirect short URLs

--- express_server.js
+++ express_server.js
@@ -46,10 +46,10 @@ app.get("/urls/:shortURL", (req, res) => {
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL; 
-  res.redirect(`/url/${shortURL}`);
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  // const longURL = ...
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
