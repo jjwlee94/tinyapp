b8cab18b1651d70d16b815d80f966755d3b39ec1
Jeewon Lee <jeewon.jlee@gmail.com>
2021-11-11 21:13:19 +0000

    Refactor helper functions

--- helpers.js
+++ helpers.js
@@ -1,3 +1,25 @@
+const urlDatabase = {};
+const users = {};
+
+const generateRandomString = function () {
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
+
+const userEmailExists = function (email, database) {
+  for (let user in database) {
+    if (database[user].email === email) {
+      return true;
+    }
+  }
+  return false;
+};
+
 const getUserByEmail = function (email, database) {
   for (let user in database) {
     if (database[user].email === email) {
@@ -6,4 +28,21 @@ const getUserByEmail = function (email, database) {
   }
 };
 
-module.exports = { getUserByEmail };
+const urlsForUser = function (id) {
+  const userURLs = {};
+  for (let shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) {
+      userURLs[shortURL] = urlDatabase[shortURL];
+    }
+  }
+  return userURLs;
+};
+
+module.exports = {
+  generateRandomString,
+  userEmailExists,
+  getUserByEmail,
+  urlsForUser,
+  urlDatabase,
+  users,
+};
